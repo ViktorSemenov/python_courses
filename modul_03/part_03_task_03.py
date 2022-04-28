b3bdf4ebda186dbda5eb06c55d3629045cc83b54
Viktor <wert-90@inbox.ru>
2022-04-28 12:40:25 +0300

    'upd_task_3_part_3'

--- modul_03/part_03_task_03.py
+++ modul_03/part_03_task_03.py
@@ -1,12 +1,15 @@
-a = [9, 8, 873, 86, 91, 919]
-#b = list(map(str, a))
-print(a)
-a.sort(key=lambda x: int(str(x)[0]))
-print(a)
 
-#k = ['1', '2', '3', '4', '987', '10', '91', '9', '13', '20', '22', '30']
-#print(sorted(k))
+def max_number(b):
+    from itertools import permutations
+    b = list(permutations(b))
+    a1 = []
+    for i in b:
+        v = list(map(str, i))
+        c = ''.join(v)
+        a1.append(int(c))
+    print(max(a1))
 
-
-
-
+a = [97867, 8, 873, 86, 91, 919]
+b = [234, 1000, 919, 7, 9, 78]
+max_number(a)
+max_number(b)
